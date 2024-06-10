a83d0710da7233870773e9572d24a3822df2015d
RaphaCosil <Raphael.Cosil@gmail.com>
2024-06-10 16:50:36 -0300

    Add - Activitys for project

---
+++ FilesCompressor/app/src/main/java/com/example/filescompressor/view/VideoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.filescompressor.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.filescompressor.R
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
